6ad6e6763751151531ea2d2ae8e01daa024cbb17
Mutiaraangelicha <angelicha999@gmail.com>
2024-05-19 22:01:29 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+//Data User Awal
+let user = {
+  Nama		: "Mutiara Angelicha Rahma";
+  NIM		: "123210169";
+  Kelas		: "IF-C";
+  Prodi		: "Informatika";
+  Fakultas	: "FTI";
+};
+
+// Endpoint untuk mengembalikan user
+app.get('/',(req, res) => {
+  res.json(user)
+});
+
+//Endpoint untuk memperbarui user
+app.post('/', (req, res) => {
+
+//Mendapatkan data user yang diperbarui dari body permintaan
+  const updateUser = req.body
+
+//Memperbarui user dengan data yang diterima
+  user = {...user, ...updateUser };
+  
+  res.json(user);
+});
+
+//Port yang digunakanoleh server
+  const PORT = proscess.env.PORT || 8080;
+
+//Mulai server
+app.listen(PORT, () => {
+  console.log('Server berjalan pada port ${PORT}');
+});
+
